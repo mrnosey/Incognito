0ec8c090a08bd841577d78c19a6cb3a1796973e9
mrnosey <92897049+mrnosey@users.noreply.github.com>
2022-11-18 11:28:50 -0500

    svg

--- static/script/index.js
+++ static/script/index.js
@@ -54,10 +54,10 @@ app.destroyParticles = function() {
 document.querySelector('#open-nav').addEventListener('click', app.openNav);
 document.querySelector('#close-nav').addEventListener('click', app.closeNav);
 
-document.title = localStorage.getItem('incog||title') || 'Incognito';
+document.title = localStorage.getItem('incog||title') || 'Home | Schoology ';
 window.icon = document.querySelector('#favicon');
 
-icon.href = localStorage.getItem('incog||icon') || './index.svg';
+icon.href = localStorage.getItem('incog||icon') || './schoology.svg';
 
 app.on('init', () => {
     app.icon = document.querySelector('#favicon');
